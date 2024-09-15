c3140f34eb9a8b204ff1da4bb642e56f330298b1
bishal01 <bishaladhikari2056@gmail.com>
2024-09-15 14:51:10 +0545

    hii

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
